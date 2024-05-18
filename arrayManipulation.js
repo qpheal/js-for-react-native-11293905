e0d7608fc4aab522a12f382ad38d8b64d39763f4
Adjei Rapheal <radjei119@st.ug.edu.gh>
2024-05-18 13:13:01 +0000

    Added the format array strings

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,5 @@
+//Task 1
+// Function to process the array of numbers
 function processArray(numbers){
     return numbers.map(number => {if (number % 2 === 0){
        return number * number
@@ -11,3 +13,25 @@ let age = [3,2,4,1]
 let processedAge = processArray(age)
 console.log(processedAge)
 //[ 9, 4, 16, 3 ]
+
+
+// Task 2
+function formatArrayStrings(strings, processedNumbers){
+    console.log(processedNumbers)
+    if (strings.length !== processedNumbers.length) {
+        throw new Error("Both arrays must have the same length.");
+    }
+    
+    return strings.map((string, index) => {
+        if (processedNumbers[index] % 2 === 0) {
+            // Capitalize the entire string if the number is even
+            return string.toUpperCase();
+        } else {
+            // Convert the string to lowercase if the number is odd
+            return string.toLowerCase();
+        }
+    });
+}
+
+let name = ["Carl", "Pheal", "Rema", "Mimi"]
+console.log(formatArrayStrings(name, processedAge))
